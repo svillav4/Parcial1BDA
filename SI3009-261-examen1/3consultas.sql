39f1f573d1cfc8e2f0807c7a8b18d980240e0084
TheSamVil <svillav4@eafit.edu.co>
2026-02-11 11:34:01 -0500

    examen 1 finalizado

--- SI3009-261-examen1/3consultas.sql
+++ SI3009-261-examen1/3consultas.sql
@@ -1,4 +1,6 @@
--- consulta 1: operativa
+-- consulta operativa
+CREATE INDEX IF NOT EXISTS operativa
+ON transactions (user_id, tx_date DESC);
 
 SELECT tx_id, amount, status
 FROM transactions
@@ -6,7 +8,10 @@ WHERE user_id = 4321
 ORDER BY tx_date DESC
 LIMIT 20;
 
--- consulta 2: analítica
+
+-- Consulta analítica
+CREATE INDEX IF NOT EXISTS analitica
+ON transactions (tx_date, user_id);
 
 SELECT country, SUM(amount)
 FROM transactions t
@@ -14,8 +19,12 @@ JOIN users u ON t.user_id = u.user_id
 WHERE tx_date >= '2024-01-01'
 GROUP BY country;
 
--- consulta 3: históricos
+
+-- Consulta históricos
+CREATE INDEX IF NOT EXISTS historica
+ON audit_log (created_at);
 
 SELECT *
 FROM audit_log
 WHERE created_at BETWEEN '2023-01-01' AND '2023-12-31';
+
